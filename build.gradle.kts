df527598627c197afc99ae57ecaffbea92cb715e
xcesoo <notxceso@gmail.com>
2025-12-05 10:59:54 +0200

    P1. Ktor setup

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass = "com.example.ApplicationKt"
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "ktor-practice-task"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
